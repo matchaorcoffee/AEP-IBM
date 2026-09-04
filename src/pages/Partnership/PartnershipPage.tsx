ff8c306819ca0e5aed1327d2bae93b3217c1e906
Shaina Nicole Jocsing <shaina.jocsing@ibm.com>
2026-09-05 01:02:42 +0800

    Fix GitHub Pages deployment

--- src/pages/Partnership/PartnershipPage.tsx
+++ src/pages/Partnership/PartnershipPage.tsx
@@ -1,14 +1,12 @@
 import styles from './PartnershipPage.module.scss'
 import partnershipImg from './assets/Partnership.png'
 import accountLeadershipImg from './assets/AccountLeadership.png'
-import projectLeadershipImg from './assets/ProjectLeadership.png'
 
 export default function PartnershipPage() {
   return (
     <div className={styles.page}>
       <img src={partnershipImg} alt="Partnership" className={styles.partnershipImg} />
       <img src={accountLeadershipImg} alt="Account Leadership" className={styles.image} />
-      <img src={projectLeadershipImg} alt="Project Leadership" className={styles.image} />
     </div>
   )
 }
