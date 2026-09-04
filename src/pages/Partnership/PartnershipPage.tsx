9c71753c1d2185d40a5943ab0b9754c7876fabf6
Shaina Nicole Jocsing <shaina.jocsing@ibm.com>
2026-09-05 06:04:58 +0800

    feat: replace AccountLeadership image with AEP Org Chart in Partnership page
    - Remove AccountLeadership.png import and image from PartnershipPage
    - Add AEP_Org_Chart_v2.html to public/ (served as static asset)
    - Embed org chart via iframe (85vh, full-width, sandbox=allow-scripts)
    - Clean up unused .image style from PartnershipPage.module.scss

--- src/pages/Partnership/PartnershipPage.tsx
+++ src/pages/Partnership/PartnershipPage.tsx
@@ -1,12 +1,18 @@
 import styles from './PartnershipPage.module.scss'
 import partnershipImg from './assets/Partnership.png'
-import accountLeadershipImg from './assets/AccountLeadership.png'
 
 export default function PartnershipPage() {
   return (
     <div className={styles.page}>
       <img src={partnershipImg} alt="Partnership" className={styles.partnershipImg} />
-      <img src={accountLeadershipImg} alt="Account Leadership" className={styles.image} />
+      <div className={styles.orgChartWrapper}>
+        <iframe
+          src="AEP_Org_Chart_v2.html"
+          className={styles.orgChartFrame}
+          title="AEP Account Org Chart"
+          sandbox="allow-scripts allow-same-origin"
+        />
+      </div>
     </div>
   )
 }
