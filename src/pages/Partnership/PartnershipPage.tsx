57c92541a3941d058275eac696b34e84736be46d
Shaina Nicole Jocsing <shaina.jocsing@ibm.com>
2026-09-05 06:29:12 +0800

    feat: remove Partnership.png from Partnership page

--- src/pages/Partnership/PartnershipPage.tsx
+++ src/pages/Partnership/PartnershipPage.tsx
@@ -1,10 +1,8 @@
 import styles from './PartnershipPage.module.scss'
-import partnershipImg from './assets/Partnership.png'
 
 export default function PartnershipPage() {
   return (
     <div className={styles.page}>
-      <img src={partnershipImg} alt="Partnership" className={styles.partnershipImg} />
       <div className={styles.orgChartWrapper}>
         <iframe
           src="AEP_Org_Chart_v2.html"
